2cb2ccc578e73191d9cadcc3c18fbccfb4b651a1
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2022-10-09 20:27:55 +0200

    Move fetch-functions to api

--- pages/api/fetch/all.js
+++ pages/api/fetch/all.js
@@ -1,6 +1,17 @@
-import { fetchAllData } from '../../../lib/scrapbox'
+import { fetchData, deleteAllData } from '../../../lib/scrapbox'
 
 export default async function handler(req, res) {
-  await fetchAllData()
+  deleteAllData()
+
+  const date = new Date()
+  const startDate = new Date(process.env.SBX_START_DATE)
+  const dayDiff = Math.floor((date - startDate) / 86400000)
+
+  for (const i of Array(dayDiff).keys()) {
+    await fetchData(date)
+    date.setDate(date.getDate() - 1)
+    console.log('progress:', i + 1, '/', dayDiff)
+  }
+
   res.status(200).json({ status: 'done' })
 }
