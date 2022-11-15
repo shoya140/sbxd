fc94d25852a925e55abaa661d65d8849fadbb131
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2022-11-15 09:59:47 +0100

    Use www.youtube-nocookie.com to embed a YouTube video

--- components/scrapbox.js
+++ components/scrapbox.js
@@ -45,7 +45,7 @@ export function Scrapbox({ node, nImages }) {
                   className="sbx-youtube-iframe"
                   width="560"
                   height="315"
-                  src={`https://www.youtube.com/embed/${youtubeKey}`}
+                  src={`https://www.youtube-nocookie.com/embed/${youtubeKey}`}
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
